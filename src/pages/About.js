e1c7ab9c5f81096cd3865f2eacc8039c49050f2e
celiltat2361 <celilbtc@gmail.com>
2021-02-20 13:50:30 +0100

    Created About.js page in pages folder.

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adip, Lorem ipsum dolor sit amet
+        Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+  )
+}
+
+export default About;
