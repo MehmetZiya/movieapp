6fd23e45b2825fb95d1602c9159d3a3ecfc87cd8
Mehmet Ziya <mehmetziya82@gmail.com>
2021-02-20 14:20:27 +0100

    add style to movieForm

--- src/pages/MovieForm.js
+++ src/pages/MovieForm.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { MovieContext } from "../contexts/MovieContext";
-
+import { inputs, formContainer } from "../styles/MovieForm.module.css"
 
 const MovieForm = () => {
   const { handleSubmit } = useContext(MovieContext)
@@ -19,17 +19,17 @@ const MovieForm = () => {
   }
 
   return (
-    <form onSubmit={(e) => handleSubmit(e, movie, history)}>
+    <form className={formContainer} onSubmit={(e) => handleSubmit(e, movie, history)}>
       <h3>Add New Movie: </h3>
-      <div>
+      <div className={inputs}>
         <input type="text" placeholder="title..." onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="director.." onChange={(e) => setDirector(e.target.value)} />
-        <input type="text" placeholder="release year.." onChange={(e) => setYear(e.target.value)} />
-        <input type="text" placeholder="genre.." onChange={(e) => setGenre(e.target.value)} />
+        <input type="text" placeholder="director..." onChange={(e) => setDirector(e.target.value)} />
+        <input type="text" placeholder="release year..." onChange={(e) => setYear(e.target.value)} />
+        <input type="text" placeholder="genre..." onChange={(e) => setGenre(e.target.value)} />
         <button type="submit">Submit</button>
       </div>
     </form>
-  )
+  );
 }
 
 export default MovieForm;
