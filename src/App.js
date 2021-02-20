b4c0e9399d272634df0e10667f1a46e95c4b0b9e
celiltat2361 <celilbtc@gmail.com>
2021-02-20 12:13:39 +0100

    added new codes in Home.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@
 function App() {
   return (
     <div className="App">
+      
       <h2>Test Header</h2>
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Hej</h2>
+    </div>
+  )
+}
+
+export default Home;
